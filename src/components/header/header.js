fb5ecf1f0eca7af6479217788e1018813c57d5cd
zhangyin01 <zhangyin01@100tal.com>
2019-08-08 18:03:16 +0800

    devin

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends React.Component {
+  render () {
+    return (
+      <div className="header">
+        <div className="header-left-column">left</div>
+      </div>
+    )
+  }
+}
+
+export default Header;
