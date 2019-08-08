69c73178482745e31924cc7101ac3759c21c9126
devin <465992294@qq.com>
2019-08-08 22:54:10 +0800

    devin

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+// import ElementUI from 'element-react';
+import 'element-theme-default';
+
 import './header.css';
 
 class Header extends React.Component {
   render () {
     return (
       <div className="header">
-        <div className="header-left-column">left</div>
+        <div className="header-left-column">
+          <span className="left-text">前端笔记</span>
+        </div>
+        <div className="header-center-column"></div>
+        <div className="header-right-column">
+          <span className="el-icon-search font-search"></span>
+          <input className="input-search" type="text" placeholder="Search..."/>
+        </div>
       </div>
     )
   }
