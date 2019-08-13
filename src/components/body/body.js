b25ad8ad68d71edd3992db56d0fb1ed10aa7aff8
devin <465992294@qq.com>
2019-08-13 21:17:21 +0800

    devin

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import LeftMenu from '../leftMenu/leftMenu';
-
+import { BrowserRouter, Route } from "react-router-dom";
+import Index from '../../pages/index/index';
+import Introduction from '../../pages/introduction/index';
 import './body.css';
 
 class Body extends React.Component {
   render () {
     return (
-      <LeftMenu></LeftMenu>
+      <div className="notes-body">
+        <LeftMenu></LeftMenu>
+        <BrowserRouter>
+          <Route path="/" exact component={Index}></Route>
+          <Route path="/Introduction" exact component={Introduction}></Route>
+        </BrowserRouter>
+      </div>
     )
   }
 }
---
+++ src/pages/index/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Index extends Component {
+  render () {
+    return (
+      <div className="index">index</div>
+    )
+  }
+}
+
+export default Index
